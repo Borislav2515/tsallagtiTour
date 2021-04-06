7dfc9c3aa5203763d2e335f88735418f044f43bc
Borislav2515 <61460569+Borislav2515@users.noreply.github.com>
2021-04-07 01:31:18 +0300

    Логика для формы

--- js/form.js
+++ js/form.js
@@ -1,23 +1,38 @@
 // форма
 
 function incr() {
+    let totalPrice = document.querySelector('.tour__price-value');
     let numGuests = document.querySelector('.price__input');
+    let startPrice = totalPrice.value;
     let x = numGuests.value;
-    if (x >= 1 || x == 0) {
+    if (x >= 1) {
         let pluseGuests = Number(x) + Number(1);
-        console.log(pluseGuests);
         numGuests.value = pluseGuests;
+        let totalResult = Number(startPrice) + Number(2000);
+        totalPrice.value = totalResult;
     }
 }
 
 function decr() {
+    let totalPrice = document.querySelector('.tour__price-value');
     let numGuests = document.querySelector('.price__input');
+    let startPrice = totalPrice.value;
     let x = numGuests.value;
-    if (x >= 1) {
+    if (x > 1) {
         let pluseGuests = Number(x) - Number(1);
-        console.log(pluseGuests);
         numGuests.value = pluseGuests;
+        let totalResult = Number(startPrice) - Number(2000);
+        totalPrice.value = totalResult;
     }
+}
+
+
+let totalPrice = document.querySelector('.tour__price-value');
+let startPrice = totalPrice.value;
+function totalPriceandCount() {
+    let numGuests = document.querySelector('.price__input');
+        let totalResult = Number(startPrice) * numGuests.value;
+        totalPrice.value = totalResult;
 }
 
 function tourdate() {
@@ -26,11 +41,12 @@ function tourdate() {
     dateIllusion.innerHTML = dateInput.value;
 }
 
-
 //маска для номера тел
 let phoneInput = document.querySelector('.input__tel');
-phoneInput.addEventListener('keydown', function(event) {
-    if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
+phoneInput.addEventListener('keydown', function (event) {
+    if (!(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) {
+        event.preventDefault()
+    }
     var mask = '+7 (111) 111-11-11'; // Задаем маску
 
     if (/[0-9\+\ \-\(\)]/.test(event.key)) {
@@ -42,7 +58,7 @@ phoneInput.addEventListener('keydown', function(event) {
             if (mask[currentLength] == '1') {
                 this.value = currentString + event.key;
             } else {
-                for (var i=currentLength; i<mask.length; i++) {
+                for (var i = currentLength; i < mask.length; i++) {
                     if (mask[i] == '1') {
                         this.value = currentString + event.key;
                         break;
